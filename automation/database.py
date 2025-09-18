de9d7e81072081224cbc8257a45b929107bc0ff9
victor <victorsonofmartin@gmail.com>
2025-09-18 17:43:53 +0300

    updated the models

--- automation/database.py
+++ automation/database.py
@@ -1,23 +1,14 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
-from icecream import ic
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+Base =  declarative_base()
 
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-ic(DATABASE_URL)
-Base =  DeclarativeBase()
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-#defint your data models
+#define your data models
 class Provider(Base):
     __tablename__ = 'providers'
     
     id = Column(Integer, primary_key=True)
-    name = Columns(String(50), nullable=False, unique=True)
+    name = Column(String(50), nullable=False, unique=True)
     email = Column(String(50), nullable=False, unique=True)
     location = Column(String(50), nullable=False, unique=True)
 
@@ -25,7 +16,7 @@ class Products(Base):
     __tablename__ = 'products'
     
     id = Column(Integer, primary_key=True)
-    name = Columns(String(50), nullable=False, unique=True)
+    name = Column(String(50), nullable=False, unique=True)
     price = Column(Integer, nullable=False)
     provider_id = Column(Integer, nullable=False)
 
